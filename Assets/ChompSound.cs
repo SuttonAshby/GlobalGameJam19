a1b4a62a783f1b6d1e026d2614e08ff33a5852e3
alexcolesound <alexcolesound@gmail.com>
2019-01-29 21:32:39 -0500

    A bunch of prefab changes. Added audiosources that weren't there before. Assigned all SFX sources to the SFX mix group and added ducking to the music group. The dinosaurs are one of our cats when she was a kitten.

---
+++ Assets/ChompSound.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChompSound : MonoBehaviour
+{
+    public void OnDestroy()
+    {
+        
+    }
+}
