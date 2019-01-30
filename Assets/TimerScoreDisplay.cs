7c95ed26e5f97bc894db28f9fe5dd7571e8d63b3
Jason Corbett <lakupo@lakupo.com>
2019-01-29 23:44:22 -0500

    Timer + Lives Display (Score not active yet), Prefabbing a few UI items

---
+++ Assets/TimerScoreDisplay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerScoreDisplay : MonoBehaviour
+{
+    public GameManager GM;
+
+    public TextMeshProUGUI ScoreDisplay;
+
+    public TextMeshProUGUI TimerDisplay;
+
+    public TextMeshProUGUI LivesDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GM = GameObject.Find("Game-Manager").GetComponent<GameManager>();
+        ScoreDisplay.text = "0";
+        LivesDisplayUpdate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimerDisplayUpdate();
+    }
+
+    void ScoreDisplayUpdate() {
+        //        ScoreDisplay.text = ""+ GM.Score;
+    }
+
+    void TimerDisplayUpdate()
+    {
+        TimerDisplay.text = ""+ GM.timeLeft.ToString("0"); // Convert float to int in display
+    }
+
+    void LivesDisplayUpdate()
+    {
+        LivesDisplay.text = ""+ GM.lives;
+    }
+}
